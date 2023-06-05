6ee5047d2de13cbe9d9494918403398fcba91b29
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-05 09:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,99 +1,128 @@
 //your JS code here. If required.
-// Get elements from the DOM
-const videoContainer = document.querySelector('.vid-container');
-const video = document.getElementById('video');
-const playerContainer = document.querySelector('.player-container');
-const audio = document.getElementById('audio');
-const soundButtons = document.querySelectorAll('.sound-picker button');
-const timeButtons = document.querySelectorAll('#time-select button');
-const timeDisplay = document.querySelector('.time-display');
-const playPauseButton = document.getElementById('play-pause');
+const app = document.getElementById('app');
 
-let interval;
+// Create video element
+const video = document.createElement('video');
+video.src = 'video/beach.mp4';
+video.autoplay = true;
+video.loop = true;
+video.muted = true;
+
+// Create audio element
+const audio = document.createElement('audio');
+audio.src = 'sounds/beach.mp3';
+audio.loop = true;
+
+// Create player container
+const playerContainer = document.createElement('div');
+playerContainer.classList.add('player-container');
+playerContainer.appendChild(audio);
+
+// Create sound picker container
+const soundPicker = document.createElement('div');
+soundPicker.classList.add('sound-picker');
+
+// Create buttons for switching between sounds
+const beachSoundBtn = document.createElement('button');
+beachSoundBtn.innerHTML = 'Beach Sound';
+beachSoundBtn.addEventListener('click', function() {
+  audio.src = 'sounds/beach.mp3';
+});
+
+const rainSoundBtn = document.createElement('button');
+rainSoundBtn.innerHTML = 'Rain Sound';
+rainSoundBtn.addEventListener('click', function() {
+  audio.src = 'sounds/rain.mp3';
+});
+
+soundPicker.appendChild(beachSoundBtn);
+soundPicker.appendChild(rainSoundBtn);
+
+// Create time buttons
+const timeSelect = document.createElement('div');
+timeSelect.id = 'time-select';
+
+const smallMins = document.createElement('button');
+smallMins.innerHTML = '2 min';
+smallMins.addEventListener('click', function() {
+  updateTime(2);
+});
+
+const mediumMins = document.createElement('button');
+mediumMins.innerHTML = '5 min';
+mediumMins.addEventListener('click', function() {
+  updateTime(5);
+});
+
+const longMins = document.createElement('button');
+longMins.innerHTML = '10 min';
+longMins.addEventListener('click', function() {
+  updateTime(10);
+});
+
+timeSelect.appendChild(smallMins);
+timeSelect.appendChild(mediumMins);
+timeSelect.appendChild(longMins);
+
+// Create time display
+const timeDisplay = document.createElement('div');
+timeDisplay.classList.add('time-display');
+timeDisplay.innerHTML = '10:00';
+
+// Create play button
+const playBtn = document.createElement('button');
+playBtn.classList.add('play');
+playBtn.addEventListener('click', function() {
+  togglePlayPause();
+});
+
+// Create progress bar
+const progressBar = document.createElement('div');
+progressBar.classList.add('progress-bar');
+const progress = document.createElement('div');
+progress.classList.add('progress');
+progressBar.appendChild(progress);
+
+// Add all elements to app container
+app.appendChild(video);
+app.appendChild(playerContainer);
+app.appendChild(soundPicker);
+app.appendChild(timeSelect);
+app.appendChild(timeDisplay);
+app.appendChild(playBtn);
+app.appendChild(progressBar);
+
+// Global variables
+let totalTime = 600;
+let timeLeft = 600;
 let isPlaying = false;
-let selectedTime = 600;
-let selectedSound = 0;
+let timerInterval;
 
-// Play or pause the audio and video
-function togglePlay() {
-    if (isPlaying) {
-        video.pause();
-        audio.pause();
-        clearInterval(interval);
-        playPauseButton.classList.remove('pause');
-        playPauseButton.classList.add('play');
-    } else {
-        video.play();
-        audio.play();
-        interval = setInterval(updateTime, 1000);
-        playPauseButton.classList.remove('play');
-        playPauseButton.classList.add('pause');
-    }
-    isPlaying = !isPlaying;
+// Function to update time display
+function updateTime(minutes) {
+  totalTime = minutes * 60;
+  timeLeft = totalTime;
+  const displayMinutes = Math.floor(totalTime / 60);
+  const displaySeconds = totalTime % 60 < 10 ? '0' + (totalTime % 60) : totalTime % 60;
+  timeDisplay.innerHTML = displayMinutes + ':' + displaySeconds;
+  if (!isPlaying) {
+    togglePlayPause();
+  }
 }
 
-// Update the time display
-function updateTime() {
-    let minutes = Math.floor(selectedTime / 60);
-    let seconds = selectedTime % 60;
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-    timeDisplay.innerText = minutes + ":" + seconds;
-    selectedTime--;
-    if (selectedTime < 0) {
-        clearInterval(interval);
-        togglePlay();
-        timeDisplay.innerText = "10:00";
-        selectedTime = 600;
-    }
+// Function to toggle play and pause
+function togglePlayPause() {
+  isPlaying = !isPlaying;
+  if (isPlaying) {
+    playBtn.innerHTML = 'Pause';
+    timerInterval = setInterval(updateTimer, 1000);
+    audio.play();
+    video.play();
+  } else {
+    playBtn.innerHTML = 'Play';
+    clearInterval(timerInterval);
+    audio.pause();
+    video.pause();
+  }
 }
 
-// Set the selected sound
-function setSound(soundIndex) {
-    selectedSound = soundIndex;
-    audio.src = "sounds/" + (selectedSound === 0 ? "beach.mp3" : "rain.mp3");
-}
-
-// Set the selected time
-function setTime(timeInSeconds) {
-    selectedTime = timeInSeconds;
-    timeButtons.forEach(button => button.classList.remove('active'));
-    if (selectedTime === 120) {
-        document.getElementById('smaller-mins').classList.add('active');
-    } else if (selectedTime === 300) {
-        document.getElementById('medium-mins').classList.add('active');
-    } else if (selectedTime === 600) {
-        document.getElementById('long-mins').classList.add('active');
-    }
-    timeDisplay.innerText = Math.floor(selectedTime / 60) + ":00";
-}
-
-// Add event listeners
-playPauseButton.addEventListener('click', togglePlay);
-
-soundButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        soundButtons.forEach(button => button.classList.remove('active'));
-        button.classList.add('active');
-        setSound(index);
-    });
-});
-
-timeButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        timeButtons.forEach(button => button.classList.remove('active'));
-        button.classList.add('active');
-        if (button.id === 'smaller-mins') {
-            setTime(120);
-        } else if (button.id === 'medium-mins') {
-            setTime(300);
-        } else if (button.id === 'long-mins') {
-            setTime(600);
-        }
-    });
-});
-
-// Set initial values
-setSound(selectedSound);
-setTime(selectedTime);
